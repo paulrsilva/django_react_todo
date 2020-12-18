b2ce6c1299fb4754c9df9be3a4da1d165b09e69d
Paul <paulino@neoplace.com.br>
2020-12-17 21:15:57 -0300

    mysql connections

--- gtd/urls.py
+++ gtd/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('api/', include('todos.api.urls')),
     path('api/auth/', include('accounts.api.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
